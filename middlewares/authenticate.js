bdc979dba3f1b1e85823e4ec8e45b4cc995e2c98
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-20 19:50:35 +0900

    feat: isAuthenticated 미들웨어 추가

---
+++ middlewares/authenticate.js
@@ -0,0 +1,29 @@
+const isAuthenticated = async (req, res, next) => {
+  const token = req.headers.authorization.split(" ")[1];
+
+  try {
+    const decodeValue = await admin.auth().verifyIdToken(token);
+
+    if (decodeValue) {
+      req.userData = decodeValue;
+
+      return next();
+    }
+
+    return res.status(401).json({ message: "Unauthorized" });
+  } catch (error) {
+    console.error("Error: ", error);
+
+    if (error instanceof admin.auth.InvalidIdTokenError) {
+      return res.status(401).json({ message: "Invalid token" });
+    }
+
+    if (error instanceof admin.auth.ExpiredIdTokenError) {
+      return res.status(401).json({ message: "Expired token" });
+    }
+
+    return res.status(500).json({ message: "Internal Error" });
+  }
+};
+
+exports.isAuthenticated = isAuthenticated;
