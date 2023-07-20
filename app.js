8b3b6b64ba471a311d13f4ed2e66a06547cd0275
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-20 22:06:00 +0900

    feat: cors를 app.js에 추가

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const createError = require("http-errors");
 const express = require("express");
 const path = require("path");
 const logger = require("morgan");
+const cors = require("cors");
 
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
@@ -16,6 +17,7 @@ mongoose
   .then(() => console.log("connected"))
   .catch(() => console.log("failed"));
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
