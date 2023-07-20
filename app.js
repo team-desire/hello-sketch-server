f0e9884d78a24c822578c620577bffd32f6c0862
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-20 21:36:34 +0900

    chore: index 라우터 제거

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const express = require("express");
 const path = require("path");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const loginRouter = require("./routes/login");
 
@@ -22,7 +21,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/login", loginRouter);
 
