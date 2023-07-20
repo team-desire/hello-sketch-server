b489d37b9d8003a2ee9d8b24ef7b07e654483a17
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-21 02:23:51 +0900

    isAuthenticated 미들웨어에서 console.error 제거

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -3,6 +3,10 @@ const admin = require("../config/firebase-config");
 const isAuthenticated = async (req, res, next) => {
   const token = req.headers.authorization;
 
+  if (!token) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
   try {
     const decodeValue = await admin.auth().verifyIdToken(token);
 
@@ -11,11 +15,7 @@ const isAuthenticated = async (req, res, next) => {
 
       return next();
     }
-
-    return res.status(401).json({ message: "Unauthorized" });
   } catch (error) {
-    console.error("Error: ", error);
-
     if (error instanceof admin.auth.InvalidIdTokenError) {
       return res.status(401).json({ message: "Invalid token" });
     }
