b2dd2cbdd4012b90d7f4932f1fcb872cb5c31c11
mangodm-web <mangodm.web3@gmail.com>
2023-07-21 16:41:05 +0900

    refactor: res.locals 제거

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,13 +1,12 @@
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 
 const errorHandler = async (err, req, res, next) => {
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-  res.locals.message = err.message || ReasonPhrases.INTERNAL_SERVER_ERROR;
+  const message = err.message || ReasonPhrases.INTERNAL_SERVER_ERROR;
 
   res.status(err.status || StatusCodes.INTERNAL_SERVER_ERROR);
   res.json({
     status: "error",
-    message: res.locals.message,
+    message,
   });
 };
 
