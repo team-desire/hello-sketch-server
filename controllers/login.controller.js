d645bfa51b1ac9f1fe69505c7d4e527bb9ab5588
jongwoobaek <qorwhddn1234@gmail.com>
2023-07-22 17:01:24 +0900

    refactor: user email 검증 로직을 login 컨트롤러에 추가

--- controllers/login.controller.js
+++ controllers/login.controller.js
@@ -4,8 +4,13 @@ const { StatusCodes, ReasonPhrases } = require("http-status-codes");
 const User = require("../models/User");
 
 exports.post = async (req, res, next) => {
+  const userEmail = req.body.email;
+
+  if (!userEmail) {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+  }
+
   try {
-    const userEmail = req.body.email;
     const existingUser = await User.findOne({ email: userEmail });
 
     if (!existingUser) {
