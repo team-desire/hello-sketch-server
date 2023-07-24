722ed128ac21fba003dbdc14db0687a2175f054c
mangodm-web <mangodm.web3@gmail.com>
2023-07-25 02:50:04 +0900

    feat: id 값 유효성 검증 유틸 함수 추가

--- utils/index.js
+++ utils/index.js
@@ -1,7 +1,17 @@
+const mongoose = require("mongoose");
+
 exports.isPageValid = (page) => {
   const parsedPage = Number(page);
 
   if (!Number.isInteger(parsedPage) || parsedPage < 1) {
+    return false;
+  }
+
+  return true;
+};
+
+exports.isIdValid = (id) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
     return false;
   }
 
