effea6403faa7061c9035b4bd529debd8b493635
mangodm-web <mangodm.web3@gmail.com>
2023-07-25 02:51:49 +0900

    feat: users 라우터 추가

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
-});
+const { getSketch } = require("../controllers/sketches.controller");
+
+router.get(`/:userId/sketches/:sketchId`, getSketch);
 
 module.exports = router;
