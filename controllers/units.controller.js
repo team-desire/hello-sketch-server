18964ea308d840681576f214d391a27ed62f230b
mangodm-web <mangodm.web3@gmail.com>
2023-07-24 17:29:28 +0900

    feat: getUnits 메소드를 units 컨트롤러에 추가

--- controllers/units.controller.js
+++ controllers/units.controller.js
@@ -1,11 +1,56 @@
 const createError = require("http-errors");
 const { StatusCodes, ReasonPhrases } = require("http-status-codes");
+
+const { getS3Client, getListObjectsCommand } = require("../utils/s3Config");
+const { isPageValid } = require("../utils");
+
 const { TEXT } = require("../constants/text");
+const { CONFIG } = require("../constants/config");
+const { OPTIONS } = require("../constants/options");
+const { NUMBER } = require("../constants/number");
 
 exports.getUnits = async (req, res, next) => {
+  const { per_page, page, type } = req.query;
+
+  if (isNaN(per_page) || isNaN(page)) {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+
+    return;
+  }
+
+  if (!isPageValid(per_page) || !isPageValid(page)) {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+
+    return;
+  }
+
+  if (!type || !OPTIONS.UNIT_TYPE.has(type)) {
+    next(createError(StatusCodes.BAD_REQUEST, ReasonPhrases.BAD_REQUEST));
+
+    return;
+  }
+
+  const perPageNumber = Number(per_page) || NUMBER.DEFAULT_ITEMS_LIMIT;
+  const pageNumber = Number(page) || NUMBER.DEFAULT_PAGE;
+
   try {
+    const objects = await getS3Objects(type);
+
+    const startIndex = (pageNumber - 1) * perPageNumber;
+    const endIndex = startIndex + perPageNumber;
+
+    const totalItems = objects.length;
+    const totalPages = Math.ceil(totalItems / perPageNumber);
+
+    const slicedObjects = objects.slice(startIndex, endIndex);
+
     res.json({
       status: TEXT.OK,
+      units: {
+        totalItems,
+        totalPages,
+        list: slicedObjects,
+      },
     });
   } catch (error) {
     next(
@@ -15,4 +60,24 @@ exports.getUnits = async (req, res, next) => {
       ),
     );
   }
+};
+
+const getS3Objects = async (type) => {
+  const s3Client = getS3Client();
+
+  const listObjectsCommand = getListObjectsCommand(
+    CONFIG.AWS_S3_BUCKET_NAME,
+    `units/${type}/`,
+    "/",
+  );
+
+  const objects = await s3Client.send(listObjectsCommand);
+
+  const filteredObjects = objects.Contents.filter(
+    (item) => !item.Key.endsWith("/"),
+  ).map((item) => ({
+    url: `https://${CONFIG.AWS_S3_BUCKET_NAME}.s3.${CONFIG.AWS_S3_REGION}.amazonaws.com/${item.Key}`,
+  }));
+
+  return filteredObjects;
 };
