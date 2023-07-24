1e67406686b00e2668d0f4dadd7729afbb0b1e85
mangodm-web <mangodm.web3@gmail.com>
2023-07-24 17:12:33 +0900

    chore: 환경 설정 관련 변수 상수화

---
+++ constants/config.js
@@ -0,0 +1,7 @@
+exports.CONFIG = {
+  MONGODB_URI: process.env.MONGODB_URI,
+  AWS_ACCESS_KEY: process.env.AWS_ACCESS_KEY,
+  AWS_SECRET_KEY: process.env.AWS_SECRET_KEY,
+  AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  AWS_S3_REGION: process.env.AWS_S3_REGION,
+};
