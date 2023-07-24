d0a6cdf3b8a87822f9c72e8828a78aff4f53772c
mangodm-web <mangodm.web3@gmail.com>
2023-07-24 17:29:05 +0900

    feat: s3 클라이언트, Objects 리스트 반환 유틸 함수 추가

---
+++ utils/s3Config.js
@@ -0,0 +1,27 @@
+const { S3Client, ListObjectsV2Command } = require("@aws-sdk/client-s3");
+
+const { CONFIG } = require("../constants/config");
+
+exports.getS3Client = () => {
+  const clientConfig = {
+    region: CONFIG.AWS_S3_REGION,
+    credentials: {
+      accessKeyId: CONFIG.AWS_ACCESS_KEY,
+      secretAccessKey: CONFIG.AWS_SECRET_KEY,
+    },
+  };
+
+  const s3Client = new S3Client(clientConfig);
+
+  return s3Client;
+};
+
+exports.getListObjectsCommand = (bucketName, prefix, delimiter) => {
+  const listObjectsCommand = new ListObjectsV2Command({
+    Bucket: bucketName,
+    Prefix: prefix,
+    Delimiter: delimiter || "/",
+  });
+
+  return listObjectsCommand;
+};
