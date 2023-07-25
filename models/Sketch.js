19a393a211c914d93bc3fb1161a66c7b57b9449e
mangodm-web <mangodm.web3@gmail.com>
2023-07-25 14:38:50 +0900

    feat: Sketch schema 수정

--- models/Sketch.js
+++ models/Sketch.js
@@ -5,10 +5,9 @@ const sketchSchema = new Schema(
   {
     userId: { type: mongoose.Types.ObjectId, ref: "User" },
     title: String,
-    type: { type: String, enum: ["basic", "cartoon"] },
+    type: { type: String, enum: ["illustration", "cartoon"] },
     isPublic: { type: Boolean, index: true },
     imageUrl: String,
-    backgroundImageUrl: String,
     canvas: {
       person: [
         {
@@ -16,20 +15,14 @@ const sketchSchema = new Schema(
             type: String,
             enum: ["head", "body", "bottom", "scene", "face"],
           },
-          zIndex: Number,
-          location: { top: String, left: String },
+          location: {
+            top: { type: String },
+            left: { type: String },
+          },
           url: String,
         },
       ],
     },
-    items: [
-      {
-        type: { type: String, enum: ["item"] },
-        zIndex: Number,
-        location: { top: String, left: String },
-        url: String,
-      },
-    ],
     comments: [{ type: mongoose.Types.ObjectId, ref: "Comment" }],
   },
   { timestamps: true },
