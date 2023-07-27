ba5e6c98a8f0a82bbb98836e9eb8071cd6a967a0
mangodm-web <mangodm.web3@gmail.com>
2023-07-28 03:18:12 +0900

    feat: userId 유효성 검증 유틸 함수 추가

--- utils/index.js
+++ utils/index.js
@@ -17,3 +17,9 @@ exports.isIdValid = (id) => {
 
   return true;
 };
+
+exports.isUserIdValid = (id) => {
+  const validFormat = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  return validFormat.test(id);
+};
