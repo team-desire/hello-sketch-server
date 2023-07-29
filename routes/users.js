821107191ddef34573dd5c6a2bc8a83423f000c3
donghyukkil <asterism90@gmail.com>
2023-07-29 15:36:17 +0900

    feat: getUserSketches를 users.js에 연결

--- routes/users.js
+++ routes/users.js
@@ -4,9 +4,11 @@ const router = express.Router();
 const {
   getSketch,
   createSketch,
+  getUserSketches,
 } = require("../controllers/sketches.controller");
 
 router.get(`/:userId/sketches/:sketchId`, getSketch);
 router.post(`/:userId/sketches`, createSketch);
+router.get(`/:userId/sketches`, getUserSketches);
 
 module.exports = router;
